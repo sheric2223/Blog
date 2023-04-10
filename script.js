225dec271a0c05e9b626cfe6fb0b062f3a55a5f5
Heric Breno Da Silva <119009016+sheric2223@users.noreply.github.com>
2023-04-10 11:09:14 -0300

    alterando js
    slide

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-function pesquisar(){
-    
+var slideItem = 0;
+window.onload = function() {
+	setInterval(passarSlide, 2000);
+
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	var objs = document.getElementsByClassName("slide");
+	for(var i=0;i<objs.length;i++) {
+		objs[i].style.width = slidewidth+"px";
+	}
+}
+function passarSlide() {
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	
+	if(slideItem >= 2) {
+		slideItem = 0;
+	} else {
+		slideItem++;
+	}
+
+	document.getElementsByClassName("sliders")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+}
+function mudarSlide(pos) {
+	slideItem = pos;
+	var slidewidth = document.getElementById("slideshow").offsetWidth;
+	document.getElementsByClassName("sliders")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
 }
