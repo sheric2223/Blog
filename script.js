53b594eb6ca6b55874a90060a1221a4a1d30ff4a
Heric Breno Da Silva <119009016+sheric2223@users.noreply.github.com>
2023-04-10 16:30:37 -0300

    ajuste
    slide

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var slideItem = 0;
 window.onload = function() {
-	setInterval(passarSlide, 2000);
+	setInterval(passarSlide, 3000);
 
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	var objs = document.getElementsByClassName("slide");
@@ -11,16 +11,16 @@ window.onload = function() {
 function passarSlide() {
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	
-	if(slideItem >= 2) {
+	if(slideItem >= 4) {
 		slideItem = 0;
 	} else {
 		slideItem++;
 	}
 
-	document.getElementsByClassName("sliders")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
 }
 function mudarSlide(pos) {
 	slideItem = pos;
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
-	document.getElementsByClassName("sliders")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
 }
