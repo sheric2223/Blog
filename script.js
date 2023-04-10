f304ccba605159acbfb63c705dd11a1d50086531
Heric Breno Da Silva <119009016+sheric2223@users.noreply.github.com>
2023-04-10 16:40:32 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var slideItem = 0;
 window.onload = function() {
-	setInterval(passarSlide, 3000);
+	setInterval(passarSlide, 5000);
 
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	var objs = document.getElementsByClassName("slide");
