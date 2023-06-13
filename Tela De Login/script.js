8db6fd0d739cf7602f9b05168092179867295306
Heric Breno Da Silva <119009016+sheric2223@users.noreply.github.com>
2023-06-13 13:53:00 -0300

    assinatura

--- Tela De Login/script.js
+++ Tela De Login/script.js
@@ -1,33 +1,14 @@
-const form = document.querySelector("#form")
 const emailInput = document.querySelector("#email")
 const passwordInput = document.querySelector("#password")
+const submit = document.querySelector("#btn")
 
-form.addEventListener("Submit",(Event) => {
-    event.preventDefault();
-    if(emailInput.value ==="" || !emailValid(emailInput.value,)) {
-        alert("Campo De Email Está Vazio");
-        return;
-    }
-    if(validadePassword.value ==="" || !validatePassword(passwordInput.value, 8)) {
-        alert("Campo De Email está vazio");
-        return;
-    }
-    form.submit();
-});
+submit.addEventListener('click',(e) => {
+    e.preventDefault()
+    const emailInputvalue = emailInput.value;
+    const passwordInputvalue = passwordInput.value;{
+    
+        if(emailInputvalue === ''|| emailInputvalue === '') {
 
-function emailValid(email){
-    const emailRegex = new RegExp(
-        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    );
-    if(emailRegex.test(email)){
-        return true;
-    }else
-        return false;
+        }
     }
-
-    function validatePassword(password, minDigits) {
-        if(password.length >=minDigits)
-        return true
-
-        return false
-    }
+})
